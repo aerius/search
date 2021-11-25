d9514c40b9431884f07a7d21236edec88b47796a
Hilbrand Bouwkamp <hilbrand@h72.nl>
2021-11-25 14:38:52 +0100

    Updated pom.xml; updated organization, added scm, added spotless, re-ordened. Ran spotless

--- search-client/src/main/java/nl/aerius/search/wui/service/SearchServiceAsyncImpl.java
+++ search-client/src/main/java/nl/aerius/search/wui/service/SearchServiceAsyncImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright the State of the Netherlands
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
 package nl.aerius.search.wui.service;
 
 import java.util.Set;
@@ -35,9 +51,10 @@ public class SearchServiceAsyncImpl implements SearchServiceAsync {
   }
 
   @Override
-  public void startSearchQuery(final String query, final Set<SearchCapability> capabilities, final String region, final String cancel, final AsyncCallback<SearchResult> callback) {
+  public void startSearchQuery(final String query, final Set<SearchCapability> capabilities, final String region, final String cancel,
+      final AsyncCallback<SearchResult> callback) {
     final String method = cancel == null ? QUERY_ASYNC_FORMAT : QUERY_ASYNC_CANCEL_FORMAT;
-    
+
     final String url = InteropRequestUtil.prepareUrl(cfg.getSearchEndpoint(), method,
         ":query", query,
         ":capabilities", capabilities.stream().map(v -> v.name()).collect(Collectors.joining(",")),
