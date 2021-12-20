63f7d21c056e64c869ac24aec53ee0fbd3f2880e
JornC <jorncruijsen@gmail.com>
2021-12-20 10:53:35 +0100

    Don't test during dev

--- scripts/search-service.sh
+++ scripts/search-service.sh
@@ -1,6 +1,6 @@
 log=$(pwd)/conf/logback.xml
 cd ..
 
-mvn clean install -pl :search-service -am
+mvn clean install -pl :search-service -am -DskipTests
 
-mvn spring-boot:run -pl :search-service -Dspring-boot.run.arguments=--logging.config=$log
+mvn spring-boot:run -pl :search-service -Dspring-boot.run.arguments=--logging.config=$log -DskipTests
