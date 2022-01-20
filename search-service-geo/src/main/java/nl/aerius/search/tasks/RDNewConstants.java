5a7df407660cc25512afba161d434caf7398bf82
Jorn C <jorncruijsen@gmail.com>
2022-01-20 18:06:24 +0100

    1.2.1 - Corrected rdnew bounds (#8)
    * Switch minY and maxX
    
    * Release 1.2.1
    
    * Release 1.2.1 for stragglers
    
    * Last one
    
    * last last one

--- search-service-geo/src/main/java/nl/aerius/search/tasks/RDNewConstants.java
+++ search-service-geo/src/main/java/nl/aerius/search/tasks/RDNewConstants.java
@@ -7,7 +7,7 @@ import nl.overheid.aerius.shared.domain.geo.ReceptorGridSettings;
 public class RDNewConstants {
   public static final int MIN_ZL_SURFACE_AREA = 10_000;
   public static final int HEX_HOR = 1529;
-  public static final BBox BOUNDS = new BBox(3604, 287959, 296800, 629300);
+  public static final BBox BOUNDS = new BBox(3604, 296800, 287959, 629300);
 
   public static ReceptorGridSettings create() {
     return ReceptorUtils.createReceptorUtil(RDNew.SRID, MIN_ZL_SURFACE_AREA, HEX_HOR, BOUNDS);
