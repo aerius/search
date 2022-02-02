8844a1ee68243fbd3002a89e04d01fdd0a2503bb
JornC <jorncruijsen@gmail.com>
2022-02-02 10:30:13 +0100

    Better remove trailing

--- scripts/remove-trailing.sh
+++ scripts/remove-trailing.sh
@@ -2,3 +2,4 @@ cd ..
 find . -name "*.java" | xargs sed -i 's/[ \t]*$//'
 find . -name "*.html" | xargs sed -i 's/[ \t]*$//'
 find . -name "*.*css" | xargs sed -i 's/[ \t]*$//'
+find . -name "*.*xml" | xargs sed -i 's/[ \t]*$//'
