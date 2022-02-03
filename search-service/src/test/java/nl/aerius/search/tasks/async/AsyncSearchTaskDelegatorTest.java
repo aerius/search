45bd3f25009617ea401c9d47f9b7e65d9070b004
JornC <jorncruijsen@gmail.com>
2022-02-03 08:44:56 +0100

    Fix tests

--- search-service/src/test/java/nl/aerius/search/tasks/async/AsyncSearchTaskDelegatorTest.java
+++ search-service/src/test/java/nl/aerius/search/tasks/async/AsyncSearchTaskDelegatorTest.java
@@ -49,6 +49,7 @@ public class AsyncSearchTaskDelegatorTest {
     final Set<SearchTaskService> tasks = Set.of(mock0Task, mock01Task, mock05Task);
 
     final TaskFactory factory = new TaskFactory(tasks);
+    factory.onFactoryConstructed();
     delegator = new AsyncSearchTaskDelegator(factory);
   }
 
