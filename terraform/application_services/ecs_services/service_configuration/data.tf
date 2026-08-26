f02bc96cc3943dbc06f69f65597f0bc1847363a4
Htet Lwin <htlttw@gmail.com>
2026-08-26 08:54:14 +0000

    Make simpler secret name and revert back to original cloud-infra-modules v1.1 - Deleted the data.tf since it is obsolete.

--- terraform/application_services/ecs_services/service_configuration/data.tf
+++
@@ -1,6 +0,0 @@
-#############################
-# COLLECT REMOTE STATE DATA #
-#############################
-
-## Bing API Key from Secrets Manager
-## Deleted since we will let the cloud-infra-modules do its magic and data.tf can also be deleted later. This is for reference.
