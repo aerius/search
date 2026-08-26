e077b48897039a63eaf1358d2c1fd881d1132ccc
htetlwin945 <89155313+htetlwin945@users.noreply.github.com>
2026-08-26 15:48:08 +0630

    Enable search to fetch bing api key from external secrects manager (#83)
    - Fetch the Bing API key from Secrects Manager instead of SSM
    - Allow `search` to fetch bing api key by using sharedSecretName
    - Deleted the data.tf since it is obsolete.

--- terraform/application_services/ecs_services/service_configuration/data.tf
+++
@@ -1,8 +0,0 @@
-#############################
-# COLLECT REMOTE STATE DATA #
-#############################
-
-## Bing API KEY
-data "aws_ssm_parameter" "bing_apikey_search" {
-  name = "/shared/keys/bing"
-}
